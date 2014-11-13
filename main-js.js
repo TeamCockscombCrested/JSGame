0833b5d95b69dacd957378b7a1f7d5068942bc07
straho99 <strahil.ruychev@gmail.com>
2014-11-13 13:12:44 +0200

    Малко анимацийка.

--- main-js.js
+++ main-js.js
@@ -1,3 +1,24 @@
 /**
  * Created by Strahil on 11/13/14.
  */
+
+function drawRectangle(left, top, width, height) {
+    //Get the context:
+    var myCanvas = document.getElementById("slot-machine");
+    var ctx = myCanvas.getContext("2d");
+
+    //Draw the rectangle:
+    ctx.fillStyle = "rgb(240,217,90)";
+    ctx.fillRect(left, top, width, height);
+}
+
+function gameLoop() {
+    var top = 400;
+
+    function frame() {
+        top -= 10;
+        drawRectangle(50, top, 50, 50);
+    }
+
+    var id = setInterval(frame, 10) // draw every 10ms
+}
